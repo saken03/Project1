2e7adde87202dc141d6559224013c0a5804db201
saken03 <54148818+saken03@users.noreply.github.com>
2022-02-25 12:32:25 +0600

    Add files via upload

---
+++ Question.java
@@ -0,0 +1,20 @@
+abstract class Question {
+	String text, answer;
+	
+	public setText(String text) {
+		this.text = text;
+	} 
+	
+	public String getText() {
+		return text;
+	}
+	
+	public setAnswer(String answer) {
+		this.answer = answer;
+	}
+	
+	public String getAnswer() {
+		return answer;
+	}
+	
+}
